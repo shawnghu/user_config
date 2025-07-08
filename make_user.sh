73309fc52a602e90d68d5a5ff5809e9d6207c496
Ubuntu <ubuntu@ip-172-31-4-246.us-east-2.compute.internal>
2025-07-08 19:21:38 +0000

    fix path

--- make_user.sh
+++ make_user.sh
@@ -1,5 +1,5 @@
 sudo adduser shawnghu sudo
-sudo -u shawnghu mkdir ~/.ssh
+sudo -u shawnghu mkdir /home/shawnghu/.ssh
 sudo cp ~/.ssh/authorized_keys /home/shawnghu/.ssh/authorized_keys
 sudo chmod 755 /home/shawnghu/.ssh/authorized_keys
 sudo chown shawnghu /home/shawnghu/.ssh/authorized_keys
