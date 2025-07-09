0d1d702d9944a5931008289dd0640d7a9c782a8e
Shawn Hu <shawnghu@gmail.com>
2025-07-09 18:43:30 +0000

    git config

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 
 command -v vim && echo "vim is installed" || sudo apt install vim || echo "vim is NOT installed"
 command -v tmux && echo "tmux is installed" || sudo apt install tmux || echo "tmux is NOT installed"
+command -v git && echo "git is installed" || sudo apt install git || echo "git is NOT installed"
 
 ln -s `pwd`/.bashrc ~
 ln -s `pwd`/.bash_profile ~
@@ -14,3 +15,6 @@ vim +PluginInstall +qall
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -s `pwd`/.tmux.conf ~
 ~/.tmux/plugins/tpm/bin/install_plugins
+
+git config --global user.email shawnghu@gmail.com 
+git config --global user.name "Shawn Hu"
