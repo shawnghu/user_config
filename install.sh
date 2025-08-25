55cc673577af2800249e49e788665df6dfde37b4
Shawn Hu <shawnghu@gmail.com>
2025-08-24 18:12:03 -0700

    new tmux logging plugin
    also rearrange install script
    was trying to automate plugin install, but turns out anish already figured it
    out for me

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # needs: vim, tmux
+command -v git && echo "git is installed" || sudo apt install git || echo "git is NOT installed"
+git config --global user.email shawnghu@gmail.com 
+git config --global user.name "Shawn Hu"
 
 sudo apt install vim-gtk3
 command -v tmux && echo "tmux is installed" || sudo apt install tmux || echo "tmux is NOT installed"
-command -v git && echo "git is installed" || sudo apt install git || echo "git is NOT installed"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s `pwd`/.tmux.conf ~
+~/.tmux/plugins/tpm/bin/install_plugins
 
 ln -s `pwd`/.bashrc ~
 ln -s `pwd`/.bash_profile ~
@@ -12,11 +17,6 @@ ln -s `pwd`/.vimrc ~
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-ln -s `pwd`/.tmux.conf ~
-~/.tmux/plugins/tpm/bin/install_plugins
 
-git config --global user.email shawnghu@gmail.com 
-git config --global user.name "Shawn Hu"
 
 
