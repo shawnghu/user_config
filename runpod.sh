a956d26ec33cad15f2e9b68e81ab498debe6be8a
Shawn Hu <shawnghu@gmail.com>
2026-01-23 22:09:26 +0000

    add less

--- runpod.sh
+++ runpod.sh
@@ -1,5 +1,5 @@
 ./make_user.sh
-sudo apt install -y ripgrep npm
+sudo apt install -y ripgrep npm less
 curl -LO https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info
 sudo tic -xe alacritty,alacritty-direct alacritty.info
 sudo sed -i 's/^#*StrictModes.*/StrictModes no/' /etc/ssh/sshd_config
