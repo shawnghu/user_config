aa327ec46662a34951d4172d49fc65ba403027a7
Shawn Hu <shawnghu@gmail.com>
2026-01-23 18:47:39 -0800

    install alacritty and make it default terminal

---
+++ setup_alacritty.sh
@@ -0,0 +1,3 @@
+sudo apt install alacritty
+gsettings set org.gnome.desktop.default-applications.terminal exec 'alacritty'
+gsettings set org.gnome.desktop.default-applications.terminal exec-arg '-e'
