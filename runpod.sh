fb6ad91787f65878671bc818a4ee43c0d1dd80e7
Shawn hu <shawnghu@gmail.com>
2026-01-28 21:31:47 +0000

    no longer install npm because claude doesn't need it; update apt

--- runpod.sh
+++ runpod.sh
@@ -1,5 +1,6 @@
 ./make_user.sh
-sudo apt install -y ripgrep npm less
+sudo apt update
+sudo apt install -y ripgrep less
 curl -LO https://raw.githubusercontent.com/alacritty/alacritty/master/extra/alacritty.info
 sudo tic -xe alacritty,alacritty-direct alacritty.info
 sudo sed -i 's/^#*StrictModes.*/StrictModes no/' /etc/ssh/sshd_config
