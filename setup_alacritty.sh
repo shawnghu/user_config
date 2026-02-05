ae351ded778e870b0ef12e3f2aaf37ff38ecaed8
Shawn Hu <shawnghu@gmail.com>
2026-02-05 01:35:15 -0800

    setup alacritty with tabbed/suckless

--- setup_alacritty.sh
+++ setup_alacritty.sh
@@ -1,3 +1,12 @@
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 sudo apt install alacritty
-gsettings set org.gnome.desktop.default-applications.terminal exec 'alacritty'
-gsettings set org.gnome.desktop.default-applications.terminal exec-arg '-e'
+
+# Build and install tabbed
+cd "$SCRIPT_DIR/tabbed" && sudo make clean install
+
+# Install wrapper script and set as default terminal
+sudo cp "$SCRIPT_DIR/tabbed-alacritty.sh" /usr/local/bin/tabbed-alacritty
+sudo chmod +x /usr/local/bin/tabbed-alacritty
+gsettings set org.gnome.desktop.default-applications.terminal exec 'tabbed-alacritty'
+gsettings set org.gnome.desktop.default-applications.terminal exec-arg ''
