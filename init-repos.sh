eea9227695b2c32889dab6dfa61437d272a1f0a5
Shawn Hu <shawnghu@gmail.com>
2026-04-16 20:29:47 +0000

    clone earlier in init repos

--- init-repos.sh
+++ init-repos.sh
@@ -5,13 +5,13 @@
 # ./install.sh
 ssh-keyscan -H github.com >> ~/.ssh/known_hosts
 cd
+git clone git@github.com:shawnghu/small-rl.git
 git clone git@github.com:ariahw/rl-rewardhacking-private.git
 cd rl-rewardhacking-private/
 source commands.sh 
 create_leetcode_dataset "simple_overwrite_tests"
 create_leetcode_dataset "simple_overwrite_tests_aware"
 cd
-git clone git@github.com:shawnghu/small-rl.git
 cd small-rl
 # git checkout worktree-scale-up
 [ -f ~/.secrets_env ] && source ~/.secrets_env
